350187c5a90fa2b9cf256ffd88c14271cab9c279
Thaís Oliveira <jts_oliveira@outlook.com>
2021-06-21 09:39:33 -0300

    code and test topping options

---
+++ src/pages/entry/ToppingOption.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+const ToppingOption = ({ name, imagePath }) => {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        alt={`${name} topping`}
+        src={`http://localhost:3030/${imagePath}`}
+      />
+    </Col>
+  );
+};
+
+export default ToppingOption;
