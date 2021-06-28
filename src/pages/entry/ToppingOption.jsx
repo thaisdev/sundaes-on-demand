0c359e77ea7f6ec62035cee65bf8dfc9626cc195
Thaís Oliveira <jts_oliveira@outlook.com>
2021-06-28 11:59:07 -0300

    test and code toopings subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.checked ? 1 : 0);
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -9,6 +13,11 @@ const ToppingOption = ({ name, imagePath }) => {
         alt={`${name} topping`}
         src={`http://localhost:3030/${imagePath}`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Col xs="6" style={{ textAlign: "left" }}>
+          <Form.Check type="checkbox" onChange={handleChange} label={name} />
+        </Col>
+      </Form.Group>
     </Col>
   );
 };
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("update scoop subtotal when scoops change", async () => {
   render(<Options optionType="scoops" />);
@@ -22,4 +21,30 @@ test("update scoop subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when scoops change", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  const mAndMsCheckbox = await screen.findByRole("checkbox", { name: "M&Ms" });
+  userEvent.click(mAndMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  const hotFudge = await screen.findByRole("checkbox", { name: "Hot fudge" });
+  userEvent.click(hotFudge);
+  expect(toppingsSubtotal).toHaveTextContent("4.50");
+
+  userEvent.click(mAndMsCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
 });
